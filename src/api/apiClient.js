4002909c19cb28d29349a724c4e0e38412b9c28a
risal-danish <risaldanish@gmail.com>
2026-05-12 09:56:13 +0530

    change baseURL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://void-server-bwya.onrender.com',
     headers:{
         'Content-Type' : 'application/json',
     }
